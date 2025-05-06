eb1cf352ecd88147f4ca71e4ef68371de0732754
Fabio Almeida <fabio.natito@gmail.com>
2025-05-06 14:38:05 -0300

    chore(#24): add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,7 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 0.1 + 0.2 as sum;");
   response.status(200).json({ chave: "Forçaaa!" });
 }
 export default status;
