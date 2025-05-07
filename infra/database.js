43fb6f9f69692978eb834c92caf280a96124a542
Fabio Almeida <fabio.natito@gmail.com>
2025-05-07 14:11:49 -0300

    fix: make `database.js`more robuts to error handling

--- infra/database.js
+++ infra/database.js
@@ -10,8 +10,13 @@ async function query(queryObject) {
   });
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 export default { query: query };
